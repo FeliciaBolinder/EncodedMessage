0d299f1d63c2e5c51ff486dda6b263f04db62652
FeliciaBolinder <43849051+FeliciaBolinder@users.noreply.github.com>
2019-05-29 11:13:51 +0200

    Add files via upload

---
+++ EncodedMessage.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EncodedMessage {
+    public static void main(String[] args) {
+
+
+        int number;
+
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter hexadecimal to convert: ");
+            String hex = dat.substring("0x" + i>7);
+            number = Integer.parseInt(sc.nextLine(), 16);
+            System.out.println(number);
+
+        }
+
+}
+
+
